72e90362e1ea27e0ce7ac03cb8ea0c50febb9b5f
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-12 19:34:20 +0200

    completato html

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,509 @@
 <script>
-// my-component.js
 export default {
   data() {
     return {
-      message: 'Template Vite + VUE'
+      linksMenu: [
+        {
+          label: 'CHARATER',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'COMICS',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'MOVIES',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'TV',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'GAMES',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'COLLECTIBLES',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'VIDEOS',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'FANS',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'NEWS',
+          url: '#',
+          active: false,
+        },
+        {
+          label: 'SHOP',
+          url: '#',
+          active: false,
+        },
+      ]
     }
-  },
+  }
 }
 </script>
 
 <template>
-  <div>
-    <h1>{{ message }}</h1>
-  </div>
+    <!-- header -->
+    <header class="container header-container">
+        <div class="box-container">
+            <div class="box-header-img">
+                <img src="../img/dc-logo.png" alt="dc-logo">
+            </div>
+            <div>
+                <ul>
+                <li>
+                    <a href="#">CHARATES</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <!-- main -->
+    <main class="container main-container">
+        --&gt; Content goes here &lt;--
+    </main>
+  
+    <!-- footer -->
+    <footer class="container">
+        <div class="box-container">
+
+            <!-- first section -->
+            <section class="footer-nav">
+              <ul>
+                  <li>
+                      <a href="#">
+                        <img src="../img/buy-comics-digital-comics.png" alt="dgital-comics">
+                        <p>DIGITAL COMICS</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#">
+                        <img src="../img/buy-comics-merchandise.png" alt="merchandise">
+                        <p>DC MERCHANDISE</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#">
+                        <img src="../img/buy-comics-subscriptions.png" alt="subscriptions">
+                        <p>SUBSCRICTION</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#">
+                        <img src="../img/buy-comics-shop-locator.png" alt="shop-locator">
+                        <p>COMIC SHOP LOCATOR</p>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#">
+                        <img src="../img/buy-dc-power-visa.png" alt="dc-power-visa">
+                        <p>DC POWER VISA</p>
+                      </a>
+                  </li>
+              </ul>
+            </section>
+
+            <!-- second section -->
+            <section class="footer-links">
+                <div class="box-footer-links">
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>DC COMICS</h4>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Charater
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Comics
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Movies
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    TV
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Games
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Videos
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    News
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>DC</h4>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Term Of Use
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Privacy Policy (New)
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Ad Choice
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Advetising
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Jobs
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Subscrictions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Talent Workshops
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    CPSC Certificates
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Talent Workshops
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Ratings
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Shop Help
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Contact Us
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>Sites</h4>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    MAD Magazine
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC Kids
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    TV
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC Universe
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    DC Power Visa
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <h4>SHOP</h4>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Shop DC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Shop DC Collectibles
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <!-- third section -->
+            <section class="social-links">
+                <div class="box-sign-up">
+                    <a href="#">
+                        SIGN-UP NOW
+                    </a>
+                </div>
+                <div>
+                    <ul>
+                        <li class="box-follow-us">
+                            <a href="#">
+                                <b>
+                                    FOLLOW US
+                                </b>
+                             </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../img/footer-facebook.png" alt="facebook">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../img/footer-periscope.png" alt="periscope">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../img/footer-pinterest.png" alt="pinterst">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../img/footer-twitter.png" alt="twitter">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="../img/footer-youtube.png" alt="youtube">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    </footer>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #303030;
+  padding: 50px 0;
+}
+
+img {
+  max-width: 100%;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  padding: 5px;
+  color: #696969;
+}
+
+a:hover {
+    color: aquamarine;
+}
+
+a:active {
+    background-color: darkgreen;
+}
+
+.box-header-img{
+    width: 5%;
+}
+
+.box-container {
+  width: 80%;
+  height: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-container {
+  height: 80px;
+  background-color: white;
+}
+
+header ul {
+  display: flex;
+}
+
+header ul li {
+  margin-left: 10px;
+}
+
+.main-container {
+    background-color: #2c67c0;
+}
+
+footer .box-container {
+    width: 100%;
+    flex-direction: column;
+    flex-wrap: wrap;
+}
+
+footer .box-container .footer-nav, footer .box-container .social-links {
+    width: 80%;
+    margin: 30px 0;
+}
+
+footer .footer-nav ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+footer .footer-nav li {
+    width: calc(100% / 10);
+}
+
+footer .footer-nav a {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+}
+
+footer .footer-links {
+    width: 100%;
+}
+
+footer .footer-links {
+    background-image: url(../img/footer-bg.jpg);
+    display: flex;
+    justify-content: center;
+    padding: 30px 0;
+}
+
+footer .footer-links .box-footer-links {
+    width: 80%;
+    display: flex;
+    gap: 40px;
+    background-image: url(../img/dc-logo-bg.png);
+    background-repeat: no-repeat;
+    background-position-x: right;
+    background-size: 20%;
+}
+
+footer .footer-links h4 {
+    color: white;
+}
+
+footer .social-links {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+footer .social-links ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+}
+
+footer .social-links ul li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+footer .social-links .box-sign-up a {
+    border: 1px solid #2c67c0;
+    color: white;
+}
+
+footer .social-links .box-follow-us a {
+    color: #2c67c0;
+    font-style: italic;
+    border: none;
+}
 </style>
